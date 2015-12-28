73e4eab1d42203de158324bea209bf091fc4d4f9
Michał Pańczyk <mjpanczyk@gmail.com>
2015-12-28 13:59:31 +0100

    Added RandomType as a superclass for every random type
    Thank to this, we can check if a type is Random
    just by writing isinstance(obj, RandomType).

--- randomtypes.py
+++ randomtypes.py
@@ -6,18 +6,21 @@ import random
 MAX_RAND_INT = 2**31
 MAX_RAND_FLOAT = 300.0
 
-class RandomInt(int):
+class RandomType(object):
+  pass
+
+class RandomInt(int, RandomType):
   def __new__(T, n=None):
     if n is None:
       n = MAX_RAND_INT
     value = random.randint(0, MAX_RAND_INT)
     return int.__new__(T, value)
 
-class RandomBool(int):
+class RandomBool(int, RandomType):
   def __new__(T):
     return int.__new__(T, random.randint(0, 1))
 
-class RandomFloat(float):
+class RandomFloat(float, RandomType):
   def __new__(T, max=MAX_RAND_FLOAT):
     value = random.random()*max
     return float.__new__(T, value)
